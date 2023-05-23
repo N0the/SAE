55adc6735084f552800d41d02018e3168d859813
Alexandre030303 <131157694+Alexandre030303@users.noreply.github.com>
2023-05-23 17:30:39 +0200

    updatefooter

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -1,8 +1,12 @@
 <footer>
-    <a href="accueil.php"><im src="public/image/noir/Logo_omnisciente_blanc.png"></a>
+    <a href="accueil.php"><img src="public/image/noir/Logo_omnisciente_blanc.png"></a>
+    <div>
+    <a href=""><img src="public/image/noir/logoyoutube.png"></a>
+    <a href=""><img src="public/image/noir/logofacebook.png"></a>
+    <a href=""><img src="public/image/noir/logoinstagram.png"></a>
+    <a href=""><img src="public/image/noir/logomail.png"></a>
     <nav>
-        <a href="accueil.php">Accueil |</a>
-        <a href=""></a>
+        <p><a href="accueil.php">Accueil</a> | <a href="boutique.php">Boutique</a> | <a href="evenement.php">Évènement</a> | <a href="jeux.php">Jeux</a> | <a href="contact.php">Contact</a></p>
     </nav>
 </footer>
 </body>
