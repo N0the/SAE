81c8a3e4df4505884385a2c7f49f4015976793ad
N0the <julien.leeder@gmail.com>
2023-05-24 11:46:54 +0200

    24-05-23

---
+++ app/view/common/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang = "fr">
+
+<head>
+    <meta charset = "UTF-8">
+    <link rel ="stylesheet" href="public/css/main.css">
+    <link rel="preconnect" href="https://www.dafont.com/fr/">
+    <link rel="stylesheet" href="https://www.dafont.com/fr/victorian-decade.font">
+    <link rel="stylesheet" href="https://www.dafont.com/fr/tt-prosto-sans.font">
+    <title><?= $titre ?></title>
+
+</head>
+
+<body>
+    <header>
+        <?php include 'app/view/common/navigation.php'?>
+    </header>
+
---
+++ boutique.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+require_once 'config.php';
+require_once 'app\model\databaseConnection.php';
+
+if (isset($_SESSION['message'])) {
+    $message = $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+
+$databaseConnection = getDatabaseConnection();
+
+
+
+$page_title = 'Boutique';
+
+require_once "app/view\common\layout.php";
